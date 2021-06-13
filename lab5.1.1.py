0bc85aa9f0af8c506ce2d2dfd38a7b2514e15d43
meornaim <21kumeyor@gmail.com>
2021-06-13 14:42:28 +0000

    Second commit lab 5

---
+++ lab5.1.1.py
@@ -0,0 +1,19 @@
+mport socket
+
+s = socket.socket()
+
+port = 8888
+
+s.connect(('192.168.56.109', port))
+
+data = s.recv(1024)
+
+s.send(b'Hi, saya client. Terima Kasih!');
+
+print (data)
+
+s.close()
+
+
+
+
